38d530e7608cfdd93e4ea694e0ad3362152a2b4f
yourssincerely <rubengoesfreelance@gmail.com>
2026-02-27 21:19:37 +0100

    programs.dms configuration added

--- modules/home/emmanuel/dms.nix
+++ modules/home/emmanuel/dms.nix
@@ -5,5 +5,19 @@
     inputs.dms.homeModules.dank-material-shell
   ];
 
-  programs.dank-material-shell.enable = true;
+  programs = {
+    dank-material-shell = {
+      enable = true;
+      package = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      systemd = {
+        enable = true;
+        restartIfChanged = true;
+      };
+      enableSystemMonitoring = true;
+      enableVPN = true;
+      enableDynamiTheming = true;
+      enableCalendarEvents = true;
+      enableClipboardPaste = true;
+    };
+  };
 }
