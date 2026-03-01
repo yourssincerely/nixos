034912d98a4b5f2bd83ab604b0e6825971d72491
yourssincerely <rubengoesfreelance@gmail.com>
2026-03-01 14:53:59 +0100

    back to ly

--- hosts/enduser/laptop/configuration.nix
+++ hosts/enduser/laptop/configuration.nix
@@ -75,18 +75,7 @@
   services = {
     blueman.enable = true;
     dbus.enable = true;
-    displayManager.dms-greeter = {
-      enable = true;
-      compositor.name = "niri";
-    };
-    greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          user = "emmanuel";
-        };
-      };
-    };
+    displayManager.ly.enable = true;
     pipewire = {
       enable = true;
       alsa.enable = true;
@@ -94,7 +83,6 @@
       pulse.enable = true;
     };
     power-profiles-daemon.enable = true;
-    seatd.enable = true;
     upower.enable = true;
   };
 
