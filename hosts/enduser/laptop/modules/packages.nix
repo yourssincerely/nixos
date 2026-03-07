554881176c9ddbb06b7e10eb63a205ce24d2b87a
yourssincerely <rubengoesfreelance@gmail.com>
2026-03-07 11:46:23 +0100

    Added environment.systemvariables and cursor to niri/config.kdl

--- hosts/enduser/laptop/modules/packages.nix
+++ hosts/enduser/laptop/modules/packages.nix
@@ -1,28 +1,35 @@
 { inputs, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    google-chrome
-    ffmpeg
-    niri
-    ghostty
-    cups-pk-helper
-    accountsservice
-    neovim
-    direnv
-    devenv
-    pcmanfm
-    pipewire
-    wireplumber
-    alsa-utils
-    wl-clipboard
-    wlogout
-    brightnessctl
-    blueman
-    tree
-    tuigreet
-    vlc
-    oh-my-zsh
-    nerd-fonts.jetbrains-mono
-  ];
+  environment = {
+      systemPackages = with pkgs; [
+        google-chrome
+        adawaita-icon-theme
+        ffmpeg
+        niri
+        ghostty
+        cups-pk-helper
+        accountsservice
+        neovim
+        direnv
+        devenv
+        pcmanfm
+        pipewire
+        wireplumber
+        alsa-utils
+        wl-clipboard
+        wlogout
+        brightnessctl
+        blueman
+        tree
+        tuigreet
+        vlc
+        oh-my-zsh
+        nerd-fonts.jetbrains-mono
+      ];
+    sessionVariables = {
+      XCURSOR_THEME = "Adwaita";
+      XCURSOR_SIZE = "24";
+    };
+  };
 }
